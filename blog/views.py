3df22702ce4ee0764ef0ba4e41c560568c0f35f0
sanghyun <zxd46@likelion.org>
2019-05-02 13:35:12 +0900

    first commit of thirdproject

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
+
+def home(request):
+    blogs = Blog.objects #쿼리셋(객체들의 목록) #메소드(객체들을 처리)
+    return render(request, 'home.html', {'blogs' : blogs})
